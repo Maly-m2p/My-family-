8ba14e24129319aa99b8ac782aa8c6d6708432a1
Maly-m2p <priscamud@gmail.com>
2026-08-08 16:00:07 +0300

    My family

--- script.js
+++ script.js
@@ -335,3 +335,9 @@ googleTranslateObserver.observe(
         subtree: true
     }
 );
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("mobile-open");
+});
